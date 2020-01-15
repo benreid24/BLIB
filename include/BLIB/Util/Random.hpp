9a601a03bf26aeecd0a84ee9cd54f8dca44ebd2a
Ben <reidben24@gmail.com>
2020-01-15 01:08:46 -0500

    Add Random and NonCopyable. Add some cmake files

---
+++ include/BLIB/Util/Random.hpp
@@ -0,0 +1,50 @@
+#ifndef BLIB_UTIL_RANDOM_HPP
+#define BLIB_UTIL_RANDOM_HPP
+
+#include <BLIB/Util/NonCopyable.hpp>
+
+#include <type_traits>
+#include <random>
+#include <limits>
+
+namespace bl
+{
+namespace Util
+{
+
+/**
+ * Random number generator for integral and floating point numbers
+ * 
+ * \ingroup Util
+ */
+class Random : private NonCopyable {
+public:
+    template<typename T>
+    static std::enable_if_t<std::is_integral_v<T>, T> get(T min, T max) {
+        if (min > max)
+            std::swap(min, max);
+        std::uniform_int_distribution<std::mt19937::result_type> rgen(min, max);
+        return rgen(_priv().rng);
+    }
+
+    template<typename T>
+    static std::enable_if_t<std::is_floating_point_v<T>, T> get(T min, T max) {
+        if (min > max)
+            std::swap(min, max);
+        const uint32_t range = std::numeric_limits<uint32_t>::max();
+        const T point = get<uint32_t>(0, maxint);
+        return min + point / static_cast<T>(range) * (max - min);
+    }
+
+private:
+    Random() : rng(rngdev()) {}
+    static Random& _priv() { static Random rng; return rng; }
+
+    std::random_device rngdev;
+    std::mt19937 rng;
+};
+
+}
+}
+
+#endif
