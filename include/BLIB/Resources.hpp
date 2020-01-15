867ca20fbbcf07364e15378f80facf3804caf502
Ben <reidben24@gmail.com>
2020-01-15 02:42:37 -0500

    Add Resources.hpp and update cmake

---
+++ include/BLIB/Resources.hpp
@@ -0,0 +1,11 @@
+#ifndef BLIB_RESOURCES_HPP
+#define BLIB_RESOURCES_HPP
+
+/**
+ * \defgroup Resources 
+ * \brief A collection of classes useful for managing resources, including their loading and lifetimes
+ */
+
+#include <BLIB/Resources/ResourceManager.hpp>
+
+#endif
