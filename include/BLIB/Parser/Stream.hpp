1b77eeb3b83f7ed68104876cf791f333f3929da2
Ben Reid <breid49@bloomberg.net>
2020-01-25 15:15:41 -0500

    Implement Stream

--- include/BLIB/Parser/Stream.hpp
+++ include/BLIB/Parser/Stream.hpp
@@ -16,21 +16,66 @@ namespace parser
  */
 class Stream {
 public:
+    /**
+     * @brief Construct a new Stream object
+     *
+     * @param data In memory string data
+     */
     Stream(const std::string& data);
-    Stream(std::ostream& stream);
 
+    /**
+     * @brief Construct a new Stream object
+     *
+     * @param stream An existing input stream
+     */
+    Stream(std::istream& stream);
+
+    /**
+     * @brief Returns the next character without modifying stream
+     *
+     * @return char Next character
+     */
     char peek() const;
+
+    /**
+     * @brief Returns next character and modifies stream
+     *
+     * @return char Next character
+     */
     char get();
 
+    /**
+     * @brief Marks the stream as invalid
+     *
+     */
     void invalidate();
+
+    /**
+     * @brief Tells whether the stream is valid. Takes into account invalidation and internal
+     *        stream state
+     *
+     * @return true Stream is valid
+     * @return false Stream is invlaid
+     */
     bool valid() const;
 
+    /**
+     * @brief Stream internally tracks which line it is on
+     *
+     * @return int The current line numer
+     */
     int currentLine() const;
+
+    /**
+     * @brief Stream internall tracks which column it is on
+     *
+     * @return int The current column
+     */
     int currentColumn() const;
 
 private:
     std::stringstream ss;
-    std::ostream& stream;
+    std::istream& stream;
     int curLine;
     int curCol;
     bool isValid;
--- src/Parser/Stream.cpp
+++ src/Parser/Stream.cpp
@@ -1,1 +1,43 @@
-#include <BLIB/Parser.hpp>
+#include <BLIB/Parser/Stream.hpp>
+
+namespace bl
+{
+namespace parser
+{
+Stream::Stream(const std::string& data)
+: ss(data)
+, stream(ss)
+, curLine(0)
+, curCol(0)
+, isValid(true) {}
+
+Stream::Stream(std::istream& stream)
+: stream(stream)
+, curLine(0)
+, curCol(0)
+, isValid(true) {}
+
+char Stream::peek() const { return stream.peek(); }
+
+char Stream::get() {
+    const char r = stream.get();
+    if (r == '\n') {
+        ++curLine;
+        curCol = 0;
+    }
+    else
+        ++curCol;
+    return r;
+}
+
+void Stream::invalidate() { isValid = false; }
+
+bool Stream::valid() const { return stream.good() && isValid; }
+
+int Stream::currentLine() const { return curLine; }
+
+int Stream::currentColumn() const { return curCol; }
+
+} // namespace parser
+
+} // namespace bl
