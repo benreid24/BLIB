c624d725cb383e57ba6d178c4ac078e1e6357104
Ben Reid <breid49@bloomberg.net>
2020-02-02 19:16:19 -0500

    Finish script grammar

--- src/Scripts/Parser.hpp
+++ src/Scripts/Parser.hpp
@@ -23,34 +23,79 @@ struct Parser {
 
     enum Grammar : parser::Node::Type {
         // Terminals
-        Def,
-        If,
-        While,
-        And,
-        Or,
-        Not,
-        LParen,
-        RParen,
-        LBrkt,
-        RBrkt,
-        LBrc,
-        RBrc,
-        Asign,
-        Eq,
-        Ne,
-        Gt,
-        Ge,
-        Lt,
-        Le,
-        Amp,
-        Plus,
-        Minus,
-        Mult,
-        Div,
-        Id,
+        NumLit,    // -123.5
+        StringLit, // "String"
+        Def,       // def
+        If,        // if
+        While,     // while
+        Return,    // return
+        And,       // and
+        Or,        // or
+        Not,       // not
+        LParen,    // (
+        RParen,    // )
+        LBrkt,     // [
+        RBrkt,     // ]
+        LBrc,      // {
+        RBrc,      // }
+        Asign,     // =
+        Eq,        // ==
+        Ne,        // !=
+        Gt,        // >
+        Ge,        // >=
+        Lt,        // <
+        Le,        // >=
+        Amp,       // &
+        Plus,      // +
+        Minus,     // -
+        Mult,      // *
+        Div,       // /
+        Hat,       // ^
+        Comma,     // ,
+        Term,      // ;
+        Id,        // varname
 
         // Non-Terminals
-        // TODO
+
+        // Arithmetic and Boolean Logic
+        RValue,   // Id. Id LBrkt Value RBrkt
+        TValue,   // RValue. NumLit. StringLit. Call
+        Exp,      // TValue. TValue Hat Value
+        Product,  // Exp. Product Mult Product
+        Sum,      // Product. Sum Plus Sum
+        Negation, // Sum. Not Negation
+        AndGrp,   // Negation. AndGrp And AndGrp
+        OrGrp,    // AndGrp. OrGrp Or OrGrp
+        Value,    // OrGrp. LParen Value RParen
+
+        // Asignment
+        Ref,       // Amp RValue
+        Asignment, // RValue Asign Value. RValue Asign Ref
+
+        // Function Call
+        ValueList, // Value. ValueList Comma Value
+        Call,      // Id LParen ValueList RParen. Id LParen RParen
+
+        // Conditional and Loop
+        Cond,        // LParen Value RParen
+        CondHead,    // If Cond
+        Conditional, // CondHead Statement. CondHead StmtBlock
+        LoopHead,    // While Cond
+        Loop,        // LoopHead Statement. LoopHead StmtBlock
+
+        // Statements
+        Statement, // Call. Conditional. Loop. Asignment Term. FDef
+        StmtList,  // Statement. StmtList Statement
+        StmtBlock, // LBrc StmtList RBrc
+
+        // Function Definition
+        Param,     // Comma Id
+        ParamList, // Id Param. ParamList Param
+        FHead,     // Id LParen ParamList RParen. Id LParen Id RParen. Id LParen RParen
+        Fdef,      // FHead StmtList
+
+        // Program
+        Program // StmtList
     };
 };
 
