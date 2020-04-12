2cad6409821bfc3f9e07d0c17d43a4aafaa7a454
Ben <reidben24@gmail.com>
2020-04-12 13:35:27 -0400

    Implement other LinePacker modes

--- src/GUI/Packers/LinePacker.cpp
+++ src/GUI/Packers/LinePacker.cpp
@@ -39,11 +39,15 @@ void LinePacker::pack(const sf::IntRect& rect, const std::vector<Element::Ptr>& 
         if (e->getRequisition().y > maxSize.y) maxSize.y = e->getRequisition().y;
     }
 
+    sf::Vector2i pos(rect.left, rect.top);
     if (mode == Compact) {
-        sf::Vector2i pos(rect.left, rect.top);
         for (Element::Ptr e : elems) {
             sf::Vector2i size(e->getRequisition().x, maxSize.y);
             if (dir == Vertical) size = {maxSize.x, e->getRequisition().y};
+            if (pos.x + size.x > rect.left + rect.width ||
+                pos.y + size.y > rect.top + rect.height)
+                std::cout << "Warning: LinePacker: Element minimum sizes exceed assigned "
+                             "acquisition\n";
             setAcquisition(e, {pos, size});
             if (dir == Horizontal)
                 pos.x += e->getRequisition().x;
@@ -51,7 +55,28 @@ void LinePacker::pack(const sf::IntRect& rect, const std::vector<Element::Ptr>& 
                 pos.y += e->getRequisition().y;
         }
     }
-    // TODO - other modes
+    else if (mode == Uniform || mode == Fill) {
+        sf::Vector2i size = maxSize;
+        if (mode == Fill) {
+            if (dir == Horizontal)
+                size.x = rect.width / elems.size();
+            else
+                size.y = rect.height / elems.size();
+        }
+        if (dir == Horizontal && size.x * elems.size() > rect.width ||
+            dir == Vertical && size.y * elems.size() > rect.height)
+            std::cout << "Warning: LinePacker: Acquisition for elements is not enough\n";
+
+        for (Element::Ptr e : elems) {
+            setAcquisition(e, {pos, size});
+            if (dir == Horizontal)
+                pos.x += size.x;
+            else
+                pos.y += size.y;
+        }
+    }
+    else
+        std::cout << "Error: Packer has invalid mode: " << mode << std::endl;
 }
 
 } // namespace gui
