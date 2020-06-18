070f6b4d07b8b6dc9b06bb3bc584c867e083bb1c
Ben Reid <reidben24@gmail.com>
2020-06-17 21:03:49 -0400

    Implement logger and example

---
+++ include/BLIB/Logging/Logger.hpp
@@ -0,0 +1,113 @@
+#ifndef BLIB_LOGGING_LOGGER_HPP
+#define BLIB_LOGGING_LOGGER_HPP
+
+#include <BLIB/Logging/LoggingConfig.hpp>
+#include <BLIB/Util/NonCopyable.hpp>
+#include <cstring>
+#include <sstream>
+
+namespace bl
+{
+/**
+ * @brief Actual logging class. Provides several static methods to begin log output. Instances
+ *        of Logger only live long enough to build the log line and flush it via the
+ *        LoggerConfig singleton instance
+ *
+ * @ingroup Logging
+ *
+ */
+struct Logger : public NonCopyable {
+    /**
+     * @brief Flushes the generated log line
+     *
+     */
+    ~Logger();
+
+    /**
+     * @brief Generic output operator. Any data type T that can be outputted to a std::ostream
+     *        can be outputted here
+     *
+     * @param data The object to log
+     * @return const Logger& This logger
+     */
+    template<typename T>
+    const Logger& operator<<(const T& data) const;
+
+    /**
+     * @brief Create a new Logger for logging a critical error
+     *
+     */
+    static Logger critical();
+
+    /**
+     * @brief Create a new Logger for logging an error
+     *
+     */
+    static Logger error();
+
+    /**
+     * @brief Create a new Logger for logging a warning
+     *
+     */
+    static Logger warn();
+
+    /**
+     * @brief Create a new Logger for logging a piece of info
+     *
+     */
+    static Logger info();
+
+    /**
+     * @brief Create a new Logger for logging low level debug info
+     *
+     */
+    static Logger debug();
+
+    /**
+     * @brief Create a new Logger for logging high volume low level info
+     *
+     */
+    static Logger trace();
+
+private:
+    Logger(const LoggingConfig& config, int level);
+
+    const LoggingConfig& config;
+    const int level;
+    mutable std::stringstream ss;
+};
+
+//////////////////////////// INLINE FUNCTIONS /////////////////////////////////
+template<typename T>
+const Logger& Logger::operator<<(const T& data) const {
+    ss << data;
+    return *this;
+}
+
+} // namespace bl
+
+////////////////////////////////// MACROS //////////////////////////////////////
+
+// clang-format off
+#define __FILENAME__ (std::strrchr(__FILE__, '/') ? std::strrchr(__FILE__, '/') + 1 : __FILE__)
+
+/// @brief Helper macro to log at critical level and include file, function, and line information
+#define BL_LOG_CRITICAL bl::Logger::critical() << __FILENAME__ << " " << __FUNCTION__ << "()" << ":" << __LINE__ << " - "
+
+/// @brief Helper macro to log at error level and include file, function, and line information
+#define BL_LOG_ERROR bl::Logger::error() << __FILENAME__ << " " << __FUNCTION__ << "()" << ":" << __LINE__ << " - "
+
+/// @brief Helper macro to log at warn level and include file, function, and line information
+#define BL_LOG_WARN bl::Logger::warn() << __FILENAME__ << " " << __FUNCTION__ << "()" << ":" << __LINE__ << " - "
+
+/// @brief Helper macro to log at info level and include file, function, and line information
+#define BL_LOG_INFO bl::Logger::info() << __FILENAME__ << " " << __FUNCTION__ << "()" << ":" << __LINE__ << " - "
+
+/// @brief Helper macro to log at debug level and include file, function, and line information
+#define BL_LOG_DEBUG bl::Logger::debug() << __FILENAME__ << " " << __FUNCTION__ << "()" << ":" << __LINE__ << " - "
+
+/// @brief Helper macro to log at trace level and include file, function, and line information
+#define BL_LOG_TRACE bl::Logger::trace() << __FILENAME__ << " " << __FUNCTION__ << "()" << ":" << __LINE__ << " - "
+// clang-format on
+
+#endif
---
+++ src/Logging/Logger.cpp
@@ -0,0 +1,31 @@
+#include <BLIB/Logging.hpp>
+
+#include <iostream>
+
+namespace bl
+{
+Logger Logger::critical() { return Logger(LoggingConfig::get(), LoggingConfig::Critical); }
+
+Logger Logger::error() { return Logger(LoggingConfig::get(), LoggingConfig::Error); }
+
+Logger Logger::warn() { return Logger(LoggingConfig::get(), LoggingConfig::Warn); }
+
+Logger Logger::info() { return Logger(LoggingConfig::get(), LoggingConfig::Info); }
+
+Logger Logger::debug() { return Logger(LoggingConfig::get(), LoggingConfig::Debug); }
+
+Logger Logger::trace() { return Logger(LoggingConfig::get(), LoggingConfig::Trace); }
+
+Logger::Logger(const LoggingConfig& config, int level)
+: config(config)
+, level(level) {
+    ss << config.genPrefix(level);
+}
+
+Logger::~Logger() {
+    config.lock();
+    config.doWrite(ss.str(), level);
+    config.unlock();
+}
+
+} // namespace bl
