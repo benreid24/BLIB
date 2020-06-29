40e3c6009fa2c8dae3d893209c761f62d42c53a6
Ben Reid <reidben24@gmail.com>
2020-06-29 18:04:15 -0400

    Start new Menu module for mouseless menu

---
+++ include/BLIB/Menu.hpp
@@ -0,0 +1,12 @@
+#ifndef BLIB_MENU_HPP
+#define BLIB_MENU_HPP
+
+/**
+ * @defgroup Menu
+ * @brief A mouseless menu that accepts basic inputs and defers rendering to the client
+ *
+ */
+
+// TODO - include
+
+#endif
---
+++ include/BLIB/Menu/Item.hpp
@@ -0,0 +1,16 @@
+#ifndef BLIB_MENU_ITEM_HPP
+#define BLIB_MENU_ITEM_HPP
+
+namespace bl
+{
+namespace menu
+{
+class Item {
+public:
+    // TODO
+};
+
+} // namespace menu
+} // namespace bl
+
+#endif
