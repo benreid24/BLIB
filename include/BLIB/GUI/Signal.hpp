71bc37e51e976b6a9440af528c280dc811b39144
Ben Reid <reidben24@gmail.com>
2020-12-04 19:05:25 -0500

    Make Signal generic utility and start implementing base classes for mouseless menu

--- include/BLIB/GUI/Signal.hpp
+++ include/BLIB/GUI/Signal.hpp
@@ -2,10 +2,7 @@
 #define BLIB_GUI_SIGNAL_HPP
 
 #include <BLIB/GUI/Action.hpp>
-#include <BLIB/Util/NonCopyable.hpp>
-#include <functional>
-#include <optional>
-#include <variant>
+#include <BLIB/Util/Signal.hpp>
 
 namespace bl
 {
@@ -13,67 +10,7 @@ namespace gui
 {
 class Element;
 
-/**
- * @brief Utility class to represent a signal on a gui Element. Each Element has a Signal for
- *        each Element::Action
- *
- * @ingroup GUI
- *
- */
-class Signal : public bl::NonCopyable {
-public:
-    /**
-     * @brief Callback function signature for a Signal
-     *
-     */
-    typedef std::function<void(const Action&, Element*)> Callback;
-
-    /**
-     * @brief When triggered, the Signal will set the given flag to the given value
-     *
-     * @param var The flag to set
-     * @param val The value to set the flag to
-     */
-    void willSet(bool& var, bool val = true);
-
-    /**
-     * @brief When triggered, the Signal will call the provided callback
-     *
-     * @param cb The function to call on trigger
-     */
-    void willCall(Callback cb);
-
-    /**
-     * @brief Set up a callback for always being called. Intended to be used by derived classes
-     *        Callbacks set by this are not cleared by clear()
-     * 
-     * @param cb The method to call when the signal is fired
-     */
-    void willAlwaysCall(Callback cb);
-
-    /**
-     * @brief Clears any set or call actions
-     *
-     */
-    void clear();
-
-    /**
-     * @brief Triggers the signal. This may be manually done to simulate input
-     *
-     * @param action The action that triggered the Singal
-     * @param id The Element that triggered the Signal
-     */
-    void operator()(const Action& action, Element* element);
-
-private:
-    Signal() = default;
-
-    std::vector<std::pair<bool*, bool>> setActions;
-    std::vector<Callback> userCallbacks;
-    std::vector<Callback> internalCallbacks;
-
-    friend class Element;
-};
+using Signal = bl::Signal<const Action&, Element*>;
 
 } // namespace gui
 } // namespace bl
--- include/BLIB/Menu/Item.hpp
+++ include/BLIB/Menu/Item.hpp
@@ -1,13 +1,32 @@
 #ifndef BLIB_MENU_ITEM_HPP
 #define BLIB_MENU_ITEM_HPP
 
+#include <BLIB/Menu/RenderItem.hpp>
+#include <memory>
+
 namespace bl
 {
 namespace menu
 {
+/**
+ * @brief Item for a mouseless menu. Can be 'attached' to other items in a Menu,
+ *        configured to be selectable, and can call a callback if selected.
+ * 
+ * @ingroup Menu
+ * 
+ */
 class Item {
 public:
-    // TODO
+    typedef std::shared_ptr<Item> Ptr;
+
+    /**
+     * @brief Points where other items can be attached
+     *
+     */
+    enum AttachPoints { Top, Right, Bottom, Left, _NUM };
+
+private:
+    Ptr attachments[_NUM];
 };
 
 } // namespace menu
