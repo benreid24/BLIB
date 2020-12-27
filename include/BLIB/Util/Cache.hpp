df1ad590bf84a66299a9e6f25b7bd11085d7d47e
Ben Reid <breid49@bloomberg.net>
2020-12-27 00:19:19 -0500

    Implement and test cache

--- include/BLIB/Util/Cache.hpp
+++ include/BLIB/Util/Cache.hpp
@@ -1,7 +1,9 @@
 #ifndef BLIB_UTIL_CACHE_HPP
 #define BLIB_UTIL_CACHE_HPP
 
+#include <BLIB/Logging.hpp>
 #include <BLIB/Util/FastQueue.hpp>
+
 #include <forward_list>
 #include <memory>
 #include <unordered_map>
@@ -50,6 +52,15 @@ public:
     bool add(const TKey& key, const Item& item);
 
     /**
+     * @brief Add an item to the cache. Will fail if the key is already in use
+     *
+     * @param key Key of the item to store
+     * @param item The item to store
+     * @return True if the item was added, false otherwise
+     */
+    bool add(const TKey& key, const TPayload& item);
+
+    /**
      * @brief Returns the storage object to allow calling code to take ownership of the stored
      *        item, if found
      *
@@ -82,14 +93,160 @@ public:
      */
     bool erase(const TKey& key);
 
+    /**
+     * @brief Returns whether or not the cache is empty
+     *
+     */
+    bool empty() const;
+
+    /**
+     * @brief Returns how many elements are currently contained
+     *
+     */
+    size_t size() const;
+
+    /**
+     * @brief Returns an iterator to the first element in the cache
+     *
+     */
+    typename std::unordered_map<TKey, TPayload>::iterator begin();
+
+    /**
+     * @brief Returns an iterator to the last element in the cache
+     *
+     */
+    typename std::unordered_map<TKey, TPayload>::iterator end();
+
 private:
     const EvictionPolicy evictionPolicy;
     const unsigned int highWatermark, lowWatermark;
 
     std::unordered_map<TKey, std::shared_ptr<TPayload>> cache;
-    FastQueue<TKey> readQueue;
+    mutable FastQueue<TKey> readQueue;
     FastQueue<TKey> writeQueue;
+    FastQueue<TKey> orderQueue;
+
+    void checkEviction();
 };
+
+//////////////////////////// INLINE FUNCTIONS /////////////////////////////////
+
+template<typename TKey, typename TPayload>
+Cache<TKey, TPayload>::Cache(EvictionPolicy policy, unsigned int lowWatermark,
+                             unsigned int highWatermark)
+: evictionPolicy(policy)
+, lowWatermark(lowWatermark)
+, highWatermark(highWatermark) {}
+
+template<typename TKey, typename TPayload>
+bool Cache<TKey, TPayload>::add(const TKey& key, const TPayload& value) {
+    return add(key, Item(new TPayload(value)));
+}
+
+template<typename TKey, typename TPayload>
+bool Cache<TKey, TPayload>::add(const TKey& key, const Item& value) {
+    if (cache.find(key) == cache.end()) {
+        cache.emplace(key, value);
+        readQueue.push_front(key);
+        writeQueue.push_front(key);
+        orderQueue.push_front(key);
+        checkEviction();
+        return true;
+    }
+    return false;
+}
+
+template<typename TKey, typename TPayload>
+typename Cache<TKey, TPayload>::Item Cache<TKey, TPayload>::getForOwn(const TKey& key) {
+    auto it = cache.find(key);
+    if (it != cache.end) {
+        readQueue.promote_front(key);
+        writeQueue.promote_front(key);
+        return it->second;
+    }
+}
+
+template<typename TKey, typename TPayload>
+const TPayload* Cache<TKey, TPayload>::getForRead(const TKey& key) const {
+    auto it = cache.find(key);
+    if (it != cache.end()) {
+        readQueue.promote_front(key);
+        return it->second.get();
+    }
+    return nullptr;
+}
+
+template<typename TKey, typename TPayload>
+TPayload* Cache<TKey, TPayload>::getForWrite(const TKey& key) {
+    auto it = cache.find(key);
+    if (it != cache.end()) {
+        readQueue.promote_front(key);
+        writeQueue.promote_front(key);
+        return it->second.get();
+    }
+    return nullptr;
+}
+
+template<typename TKey, typename TPayload>
+bool Cache<TKey, TPayload>::erase(const TKey& key) {
+    auto it = cache.find(key);
+    if (it != cache.end()) {
+        cache.erase(it);
+        readQueue.remove(key);
+        writeQueue.remove(key);
+        orderQueue.remove(key);
+        return true;
+    }
+    return false;
+}
+
+template<typename TKey, typename TPayload>
+typename std::unordered_map<TKey, TPayload>::iterator Cache<TKey, TPayload>::begin() {
+    return cache.begin();
+}
+
+template<typename TKey, typename TPayload>
+typename std::unordered_map<TKey, TPayload>::iterator Cache<TKey, TPayload>::end() {
+    return cache.begin();
+}
+
+template<typename TKey, typename TPayload>
+bool Cache<TKey, TPayload>::empty() const {
+    return cache.empty();
+}
+
+template<typename TKey, typename TPayload>
+size_t Cache<TKey, TPayload>::size() const {
+    return cache.size();
+}
+
+template<typename TKey, typename TPayload>
+void Cache<TKey, TPayload>::checkEviction() {
+    if (cache.size() >= highWatermark) {
+        FastQueue<TKey>* queue = nullptr;
+        switch (evictionPolicy) {
+        case LeastRecentlyAccessed:
+            queue = &readQueue;
+            break;
+        case LeastRecentlyModified:
+            queue = &writeQueue;
+            break;
+
+        case FirstInFirstOut:
+            queue = &orderQueue;
+            break;
+        default:
+            BL_LOG_ERROR << "Invalid eviciton policy in queue: " << evictionPolicy;
+            return;
+        }
+
+        while (cache.size() > lowWatermark && !queue->empty()) {
+            TKey key;
+            queue->pop_back(key);
+            cache.erase(key);
+        }
+    }
+}
 
 } // namespace bl
 
