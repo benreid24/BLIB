55f3e09765a012e3c669e349d4120e368838a8eb
Ben Reid <reidben24@gmail.com>
2021-02-03 13:36:29 -0500

    Implement and test FastEraseVector

---
+++ tests/Containers/FastEraseVector.t.cpp
@@ -0,0 +1,25 @@
+#include <BLIB/Containers/FastEraseVector.hpp>
+#include <gtest/gtest.h>
+
+namespace bl
+{
+namespace unittest
+{
+TEST(FastEraseVector, Erase) {
+    FastEraseVector<int> vec;
+
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    vec.erase(0);
+    vec.erase(vec.begin() + 2);
+
+    EXPECT_EQ(vec[0], 5);
+    EXPECT_EQ(vec[1], 2);
+    EXPECT_EQ(vec[2], 4);
+}
+
+} // namespace unittest
+} // namespace bl
