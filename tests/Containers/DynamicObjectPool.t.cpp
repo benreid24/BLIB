43f6dc1678625cd1e43d4190ea4652b1fa416924
Ben Reid <reidben24@gmail.com>
2021-03-04 18:53:15 -0500

    Add clear() and shrink() to dynamic object pool. Implement proper move and destruct semantics

--- tests/Containers/DynamicObjectPool.t.cpp
+++ tests/Containers/DynamicObjectPool.t.cpp
@@ -5,6 +5,22 @@ namespace bl
 {
 namespace unittest
 {
+namespace
+{
+struct Dummy {
+    Dummy()
+    : d(nullptr) {}
+
+    ~Dummy() {
+        if (d) *d = true;
+    }
+
+    void set(bool* _d) { d = _d; }
+
+    bool* d;
+};
+} // namespace
+
 TEST(DynamicObjectPool, Empty) {
     DynamicObjectPool<int> pool;
     EXPECT_TRUE(pool.empty());
@@ -90,6 +106,84 @@ TEST(DynamicObjectPool, Reuse) {
     EXPECT_EQ(*(pool.begin() + 2), 3);
     EXPECT_EQ(*(pool.begin() + 3), 20);
     EXPECT_EQ(*(pool.begin() + 4), 5);
+}
+
+TEST(DynamicObjectPool, Destructor) {
+    DynamicObjectPool<Dummy> pool;
+    bool dcalls[] = {false, false, false};
+
+    pool.add({});
+    pool.add({});
+    pool.add({});
+
+    pool.begin()->set(&dcalls[0]);
+    (++pool.begin())->set(&dcalls[1]);
+    (pool.begin() + 2)->set(&dcalls[2]);
+
+    ASSERT_FALSE(dcalls[0]);
+    ASSERT_FALSE(dcalls[1]);
+    ASSERT_FALSE(dcalls[2]);
+
+    pool.erase(pool.begin() + 1);
+    ASSERT_FALSE(dcalls[0]);
+    ASSERT_TRUE(dcalls[1]);
+    ASSERT_FALSE(dcalls[2]);
+
+    pool.clear();
+    ASSERT_EQ(pool.size(), 0);
+    EXPECT_EQ(pool.capacity(), 3);
+    ASSERT_TRUE(dcalls[0]);
+    ASSERT_TRUE(dcalls[1]);
+    ASSERT_TRUE(dcalls[2]);
+
+    pool.clear(true);
+    EXPECT_EQ(pool.capacity(), 0);
+}
+
+TEST(DynamicObjectPool, Shrink) {
+    DynamicObjectPool<int> pool;
+
+    pool.add(1);
+    pool.add(2);
+    pool.add(3);
+    pool.erase(pool.begin() + 1);
+
+    ASSERT_EQ(pool.size(), 2);
+    ASSERT_EQ(pool.capacity(), 3);
+
+    pool.shrink();
+    EXPECT_EQ(pool.capacity(), 2);
+    EXPECT_EQ(*pool.begin(), 1);
+    EXPECT_EQ(*(pool.begin() + 1), 3);
+}
+
+TEST(DynamicObjectPool, ShrinkDestructor) {
+    DynamicObjectPool<Dummy> pool;
+    bool dcalls[] = {false, false, false};
+
+    pool.add({});
+    pool.add({});
+    pool.add({});
+
+    pool.begin()->set(&dcalls[0]);
+    (++pool.begin())->set(&dcalls[1]);
+    (pool.begin() + 2)->set(&dcalls[2]);
+
+    ASSERT_FALSE(dcalls[0]);
+    ASSERT_FALSE(dcalls[1]);
+    ASSERT_FALSE(dcalls[2]);
+
+    pool.erase(pool.begin() + 1);
+    ASSERT_FALSE(dcalls[0]);
+    ASSERT_TRUE(dcalls[1]);
+    ASSERT_FALSE(dcalls[2]);
+
+    pool.shrink();
+    EXPECT_EQ(pool.capacity(), 2);
+
+    ASSERT_FALSE(dcalls[0]);
+    ASSERT_TRUE(dcalls[1]);
+    ASSERT_FALSE(dcalls[2]);
 }
 
 } // namespace unittest
