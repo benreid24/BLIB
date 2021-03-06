1bcaac6cb2122c4e93dd417b9ba34c4a43d79909
Ben Reid <reidben24@gmail.com>
2021-03-05 23:00:29 -0500

    Create Events doc group

--- include/BLIB/Events.hpp
+++ include/BLIB/Events.hpp
@@ -1,6 +1,12 @@
 #ifndef BLIB_EVENTS_HPP
 #define BLIB_EVENTS_HPP
 
+/**
+ * @defgroup Events
+ * @brief Collection of utility classes for pub/sub style event queues
+ *
+ */
+
 #include <BLIB/Events/EventDispatcher.hpp>
 #include <BLIB/Events/EventDispatcherScopeGuard.hpp>
 #include <BLIB/Events/EventListener.hpp>
