5d5027b34fa37d78352aea2e916e6fb3cfd28716
Ben Reid <reidben24@gmail.com>
2021-04-16 22:19:24 -0400

    Improve configuration interface and add saving/loading. Add unit tests around configuration

--- include/BLIB/Engine/Configuration.hpp
+++ include/BLIB/Engine/Configuration.hpp
@@ -16,7 +16,9 @@ namespace engine
 /**
  * @brief Global typeless configuration store for engine settings. Applications may add their own
  *        configuration settings to the store as well. Some examples of configuration are the path
- *        prefixes for songs in Playlists and spritesheets in Animations
+ *        prefixes for songs in Playlists and spritesheets in Animations. Reading the configuration
+ *        is threadsafe, however all calls to set() should be performed before any concurrent
+ *        access is attempted
  *
  * @ingroup Engine
  *
@@ -30,21 +32,75 @@ public:
      * @tparam T The type of the item to be retrieved
      * @param key The key of the item to be returned
      * @param defaultValue The default value to return if not found
+     * @return T The stored configuration value
+     */
+    template<typename T>
+    static T getOrDefault(const std::string& key, const T& defaultValue = {});
+
+    /**
+     * @brief Returns the stored value for the given configuration type and key. If the key does not
+     *        exist or is a different type then the default constructed value is returned
+     *
+     * @tparam T The type of the item to be retrieved
+     * @param key The key of the item to be returned
      * @return const T& The stored configuration value
      */
     template<typename T>
-    static const T& get(const std::string& key, const T& defaultValue = T());
+    static const T& get(const std::string& key);
 
     /**
      * @brief Sets the value for the given type and key in the config store. Keys may be duplicated
-     *        across types. Values may not be overwritten
+     *        across types. Values may not be overwritten by default
      *
      * @tparam T The type of item to set
      * @param key The key to set the value for
      * @param value The value to set the config to
+     * @param overwrite Set to true to overwrite existing value
      */
     template<typename T>
-    static void set(const std::string& key, const T& value);
+    static void set(const std::string& key, const T& value, bool overwrite = false);
+
+    /**
+     * @brief Loads the configuration from the given file. Default values should be set for all
+     *        expected keys first, then this should be called. Existing keys are maintained but
+     *        overwritten if a value is specified in the file
+     *
+     * @details
+     * The configuration file is a plain text file in the format
+     *
+     *     <type><key>=<value>
+     *     <type><key>=<value>
+     *     ...
+     *
+     * Where <type> is one of:
+     *     b - boolean value. "true" and "false" are the expected values
+     *     i - integer value. Loaded in with int as the type key
+     *     u - unsigned integer. Loaded in with unsigned int as the type key
+     *     f - floating point number. Loaded in with float as the type key
+     *     s - String value
+     *
+     * <key> is any string not containing the '=' character
+     * <value> is any the text between '=' and '\n' and will be parsed as the specified type
+     *
+     * @param file The file to load from
+     * @return True if loaded without error, false if any error. Default values still present
+     */
+    static bool load(const std::string& file);
+
+    /**
+     * @brief Saves the configuration to the given file. See load() for the file format
+     * @see load
+     *
+     * @param file The file to save to
+     * @return True on successful write, false for any error
+     */
+    static bool save(const std::string& file);
+
+    /**
+     * @brief Clears out all stored config data. Useful for doing a fresh reload
+     *
+     */
+    static void clear();
 
 private:
     std::unordered_map<std::type_index, std::unordered_map<std::string, std::any>> config;
@@ -57,7 +113,7 @@ private:
 //////////////////////////// INLINE FUNCTIONS /////////////////////////////////
 
 template<typename T>
-const T& Configuration::get(const std::string& key, const T& d) {
+T Configuration::getOrDefault(const std::string& key, const T& d) {
     auto& config    = get().config;
     const auto tkey = std::type_index(typeid(T));
     const auto it   = config.find(tkey);
@@ -75,7 +131,26 @@ const T& Configuration::get(const std::string& key, const T& d) {
 }
 
 template<typename T>
-void Configuration::set(const std::string& key, const T& value) {
+const T& Configuration::get(const std::string& key) {
+    auto& config    = get().config;
+    const auto tkey = std::type_index(typeid(T));
+    const auto it   = config.find(tkey);
+    if (it != config.end()) {
+        const auto jit = it->second.find(key);
+        if (jit != it->second.end()) { return *std::any_cast<T>(&jit->second); }
+    }
+
+    static std::unordered_set<std::string> warned;
+    static const T d = {};
+    if (warned.find(key) == warned.end()) {
+        warned.insert(key);
+        BL_LOG_WARN << "Using default value for unset config: " << key << " value: " << d;
+    }
+    return d;
+}
+
+template<typename T>
+void Configuration::set(const std::string& key, const T& value, bool ov) {
     auto& config    = get().config;
     const auto tkey = std::type_index(typeid(T));
 
@@ -83,19 +158,14 @@ void Configuration::set(const std::string& key, const T& value) {
     if (tit == config.end()) { tit = config.try_emplace(tkey).first; }
 
     auto it = tit->second.find(key);
-    if (it == tit->second.end()) {
+    if (it == tit->second.end() || ov) {
         BL_LOG_INFO << "Configuration set: " << key << "=" << value;
-        tit->second.emplace(key, value);
+        tit->second[key] = std::make_any<T>(value);
     }
     else {
         BL_LOG_WARN << "Tried to overwrite configuration (" << key << "="
                     << std::any_cast<T>(it->second) << ") to " << value;
     }
-}
-
-inline Configuration& Configuration::get() {
-    static Configuration configuration;
-    return configuration;
 }
 
 } // namespace engine
---
+++ tests/Engine/Configuration.t.cpp
@@ -0,0 +1,95 @@
+#include <BLIB/Engine/Configuration.hpp>
+#include <gtest/gtest.h>
+
+namespace bl
+{
+namespace engine
+{
+namespace unittest
+{
+TEST(EngineConfiguration, SingleTypeManyKeys) {
+    Configuration::clear();
+
+    Configuration::set<int>("key1", 52);
+    Configuration::set<int>("key2", -123);
+
+    EXPECT_EQ(Configuration::get<int>("key1"), 52);
+    EXPECT_EQ(Configuration::get<int>("key2"), -123);
+}
+
+TEST(EngineConfiguration, ManyTypesManyKeys) {
+    Configuration::clear();
+
+    Configuration::set<int>("key1", 124);
+    Configuration::set<std::string>("key2", "str");
+
+    EXPECT_EQ(Configuration::get<int>("key1"), 124);
+    EXPECT_EQ(Configuration::get<std::string>("key2"), "str");
+}
+
+TEST(EngineConfiguration, SingleKeyManyTypes) {
+    Configuration::clear();
+
+    Configuration::set<int>("key", 5);
+    Configuration::set<std::string>("key", "str");
+
+    EXPECT_EQ(Configuration::get<int>("key"), 5);
+    EXPECT_EQ(Configuration::get<std::string>("key"), "str");
+}
+
+TEST(EngineConfiguration, DefaultValues) {
+    Configuration::clear();
+
+    EXPECT_EQ(Configuration::getOrDefault<int>("key", 5), 5);
+    EXPECT_EQ(Configuration::get<std::string>("key"), std::string());
+}
+
+TEST(EngineConfiguration, NoOverwrite) {
+    Configuration::clear();
+
+    Configuration::set<int>("key", 5);
+    Configuration::set<int>("key", 10);
+
+    EXPECT_EQ(Configuration::get<int>("key"), 5);
+}
+
+TEST(EngineConfiguration, Overwrite) {
+    Configuration::clear();
+
+    Configuration::set<int>("key", 5);
+    Configuration::set<int>("key", 10, true);
+
+    EXPECT_EQ(Configuration::get<int>("key"), 10);
+}
+
+TEST(EngineConfiguration, Clear) {
+    Configuration::clear();
+
+    Configuration::set<int>("key", 5);
+    Configuration::clear();
+    EXPECT_EQ(Configuration::getOrDefault<int>("key", 54), 54);
+}
+
+TEST(EngineConfiguration, SaveAndLoad) {
+    Configuration::clear();
+
+    Configuration::set<int>("int", -54);
+    Configuration::set<unsigned int>("uint", 100);
+    Configuration::set<float>("float", -563.5);
+    Configuration::set<bool>("BLIB.cool", true);
+    Configuration::set<std::string>("str", "some value");
+
+    ASSERT_TRUE(Configuration::save("config.cfg"));
+    Configuration::clear();
+    ASSERT_TRUE(Configuration::load("config.cfg"));
+
+    EXPECT_EQ(Configuration::get<int>("int"), -54);
+    EXPECT_EQ(Configuration::get<unsigned int>("uint"), 100);
+    EXPECT_EQ(Configuration::get<float>("float"), -563.5);
+    EXPECT_EQ(Configuration::get<bool>("BLIB.cool"), true);
+    EXPECT_EQ(Configuration::get<std::string>("str"), "some value");
+}
+
+} // namespace unittest
+} // namespace engine
+} // namespace bl
