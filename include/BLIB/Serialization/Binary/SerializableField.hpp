3ff9baa8591a40a33e35c7ee4f211db4ffc755d0
Ben Reid <reidben24@gmail.com>
2021-12-01 22:43:28 -0500

    Add interfaces for serializable fields and objects

---
+++ include/BLIB/Serialization/Binary/SerializableField.hpp
@@ -0,0 +1,149 @@
+#ifndef BLIB_SERIALIZATION_BINARY_SERIALIZABLEFIELD_HPP
+#define BLIB_SERIALIZATION_BINARY_SERIALIZABLEFIELD_HPP
+
+#include <BLIB/Serialization/Binary/Serializer.hpp>
+#include <BLIB/Util/NonCopyable.hpp>
+#include <cstdint>
+
+namespace bl
+{
+namespace serial
+{
+namespace binary
+{
+class SerializableObjectBase;
+
+/**
+ * @brief Base class for representing serializable class members. Do not use directly
+ *
+ * @ingroup Serialization
+ *
+ */
+struct SerializableFieldBase {
+    /**
+     * @brief Construct the field with the object descriptor that it belongs to
+     *
+     * @param owner The serizable object descriptor that owns this field
+     */
+    SerializableFieldBase(SerializableObjectBase& owner);
+
+    /**
+     * @brief Returns the id of the field
+     *
+     */
+    virtual std::uint16_t id() const = 0;
+
+    /**
+     * @brief Returns the offset of the field
+     *
+     */
+    virtual std::size_t offset() const = 0;
+
+    /**
+     * @brief Serializes the object at the given address to the given stream
+     *
+     * @param stream The stream to serialize to
+     * @param object The object to serialize
+     * @return True on success, false on error
+     */
+    virtual bool serialize(OutputStream& stream, const void* object) const = 0;
+
+    /**
+     * @brief Deserializes the object at the given address to the given stream
+     *
+     * @param stream The stream to deserialize to
+     * @param object The object to deserialize
+     * @return True on success, false on error
+     */
+    virtual bool deserialize(InputStream& stream, void* object) const = 0;
+
+    /**
+     * @brief Returns the serialized size of the field in the given object
+     *
+     * @param object The object to get the field value from
+     * @return std::size_t The size of the field when serialized, in bytes
+     */
+    virtual std::size_t size(const void* object) const = 0;
+};
+
+/**
+ * @brief Descriptor class for a data member of a class to be serialized
+ *
+ * @tparam Id The id of the data member. Allows formats to change without invalidating existing data
+ * @tparam T The type of the data member
+ * @tparam Offset The offset of the data member in it's class
+ * @ingroup Serialization
+ */
+template<std::uint16_t Id, typename T, std::size_t Offset>
+struct SerializableField
+: public SerializableFieldBase
+, private bl::util::NonCopyable {
+    /**
+     * @brief Removed
+     *
+     */
+    SerializableField() = delete;
+
+    /**
+     * @brief Construct the field with the object descriptor that it belongs to
+     *
+     * @param owner The serizable object descriptor that owns this field
+     */
+    SerializableField(SerializableObjectBase& owner);
+
+    /**
+     * @brief Returns the id of the field
+     *
+     */
+    virtual std::uint16_t id() const override { return Id; }
+
+    /**
+     * @brief Returns the offset of the field
+     *
+     */
+    virtual std::size_t offset() const override { return Offset; }
+
+    /**
+     * @brief Serializes the object at the given address to the given stream
+     *
+     * @param stream The stream to serialize to
+     * @param object The object to serialize
+     * @return True on success, false on error
+     */
+    virtual bool serialize(OutputStream& stream, const void* obj) const override {
+        const void* addr = obj + Offset;
+        const T* field   = static_cast<const T*>(addr);
+        return Serializer<T>::serialize(stream, *field);
+    }
+
+    /**
+     * @brief Deserializes the object at the given address to the given stream
+     *
+     * @param stream The stream to deserialize to
+     * @param object The object to deserialize
+     * @return True on success, false on error
+     */
+    virtual void deserialize(InputStream& stream, void* object) const override {
+        void* addr = object + Offset;
+        T* field   = static_cast<T*>(addr);
+        return Serializer<T>::deserialize(stream, *field);
+    }
+
+    /**
+     * @brief Returns the serialized size of the field in the given object
+     *
+     * @param object The object to get the field value from
+     * @return std::size_t The size of the field when serialized, in bytes
+     */
+    virtual std::size_t size(const void* object) const override {
+        const void* addr = object + Offset;
+        const T* field   = static_cast<const T*>(addr);
+        return Serializer<T>::size(*field);
+    }
+};
+
+} // namespace binary
+} // namespace serial
+} // namespace bl
+
+#endif
---
+++ include/BLIB/Serialization/Binary/SerializableObject.hpp
@@ -0,0 +1,74 @@
+#ifndef BLIB_SERIALIZATION_SERIALIZABLEOBJECT_HPP
+#define BLIB_SERIALIZATION_SERIALIZABLEOBJECT_HPP
+
+#include <BLIB/Serialization/Binary/InputStream.hpp>
+#include <BLIB/Serialization/Binary/OutputStream.hpp>
+#include <unordered_map>
+
+namespace bl
+{
+namespace serial
+{
+namespace binary
+{
+struct SerializableFieldBase;
+
+/**
+ * @brief Base class for object descriptors. Provides methods used by Serializer
+ *
+ * @ingroup Serialization
+ *
+ */
+class SerializableObjectBase {
+public:
+    /**
+     * @brief Serializes the given object to the given stream
+     *
+     * @param stream The stream to serialize to
+     * @param object The object to serialize
+     * @return True on success, false on error
+     */
+    bool serialize(OutputStream& stream, const void* object) const;
+
+    /**
+     * @brief Deserializes from the stream into the given object
+     *
+     * @param stream The stream to read from
+     * @param object The object to read into
+     * @return True on success, false on error
+     */
+    bool deserialize(InputStream& stream, void* object) const;
+
+    /**
+     * @brief Returns the size of the object when serialized, in bytes
+     *
+     * @param object The object to serialize
+     * @return std::size_t The serialized size of the object, in bytes
+     */
+    std::size_t size(const void* object) const;
+
+private:
+    std::unordered_map<std::uint16_t, SerializableFieldBase*> fields;
+};
+
+/**
+ * @brief Base template for object descriptors. Specialize this for classes that you wish to
+ *        serialize. If this is instantiated without a specialization a compiler error occurs
+ *
+ * @tparam T The type of object to describe serialization for
+ */
+template<typename T>
+class SerializableObject : public SerializableObjectBase {
+    template<typename U>
+    struct MakeError : std::false_type {};
+
+    static_assert(
+        MakeError<T>::value,
+        "Type does not have object descriptor. Specialize SerializableObject or Serializer.");
+};
+
+} // namespace binary
+} // namespace serial
+} // namespace bl
+
+#endif
