75215e98de6fc2eb74dfa50eda64243834522df4
Ben Reid <reidben24@gmail.com>
2022-10-28 21:32:26 -0400

    Implement ReadWriteLock. Fix deadlock in event dispatcher

---
+++ src/Util/ReadWriteLock.cpp
@@ -0,0 +1,74 @@
+#include <BLIB/Util/ReadWriteLock.hpp>
+
+#include <unordered_map>
+
+namespace bl
+{
+namespace util
+{
+namespace
+{
+thread_local std::unordered_map<ReadWriteLock*, bool> readLockFlags;
+thread_local std::unordered_map<ReadWriteLock*, bool> writeLockFlags;
+} // namespace
+
+void ReadWriteLock::lockRead() {
+    if (readLockFlag()) return;
+    readLockFlag() = true;
+    mutex.lock_shared();
+}
+
+void ReadWriteLock::unlockRead() {
+    if (!readLockFlag()) return;
+    readLockFlag() = false;
+    mutex.unlock_shared();
+}
+
+void ReadWriteLock::lockWrite() {
+    if (writeLockFlag()) return;
+    writeLockFlag() = true;
+    mutex.lock();
+}
+
+void ReadWriteLock::unlockWrite() {
+    if (!writeLockFlag()) return;
+    writeLockFlag() = false;
+    mutex.unlock();
+}
+
+bool& ReadWriteLock::readLockFlag() { return readLockFlags[this]; }
+
+bool& ReadWriteLock::writeLockFlag() { return writeLockFlags[this]; }
+
+ReadWriteLock::ReadScopeGuard::ReadScopeGuard(ReadWriteLock& lock)
+: lock(lock)
+, unlocked(false) {
+    lock.lockRead();
+}
+
+ReadWriteLock::ReadScopeGuard::~ReadScopeGuard() { unlock(); }
+
+void ReadWriteLock::ReadScopeGuard::unlock() {
+    if (!unlocked) {
+        unlocked = true;
+        lock.unlockRead();
+    }
+}
+
+ReadWriteLock::WriteScopeGuard::WriteScopeGuard(ReadWriteLock& lock)
+: lock(lock)
+, unlocked(false) {
+    lock.lockWrite();
+}
+
+ReadWriteLock::WriteScopeGuard::~WriteScopeGuard() { unlock(); }
+
+void ReadWriteLock::WriteScopeGuard::unlock() {
+    if (!unlocked) {
+        unlocked = true;
+        lock.unlockWrite();
+    }
+}
+
+} // namespace util
+} // namespace bl
--- tests/Events/Dispatcher.t.cpp
+++ tests/Events/Dispatcher.t.cpp
@@ -2,6 +2,8 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <thread>
+
 namespace bl
 {
 namespace event
@@ -17,6 +19,29 @@ public:
     MOCK_METHOD(void, observe, (const int&), (override));
     MOCK_METHOD(void, observe, (const std::string&), (override));
 };
+
+class NestedListenerDispatcher : public Listener<int> {
+public:
+    NestedListenerDispatcher(Dispatcher& bus)
+    : bus(bus) {}
+
+    virtual ~NestedListenerDispatcher() = default;
+
+    virtual void observe(const int&) override { bus.dispatch<char>('a'); }
+
+private:
+    Dispatcher& bus;
+};
+
+void nestedTester(bool& markTrue) {
+    Dispatcher bus;
+
+    NestedListenerDispatcher guy(bus);
+    bus.subscribe(&guy);
+
+    bus.dispatch<int>(5); // to catch deadlocks
+    markTrue = true;
+}
 
 } // namespace
 
@@ -41,6 +66,14 @@ TEST(Dispatcher, Unsubscribe) {
 
     dispatch.dispatch<int>(5);
     dispatch.dispatch<std::string>("Hello");
+}
+
+TEST(Dispatcher, NestedDispatch) {
+    bool threadFinished = false;
+    std::thread runner(std::bind(&nestedTester, std::ref(threadFinished)));
+    runner.detach();
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    EXPECT_TRUE(threadFinished);
 }
 
 } // namespace unittest
