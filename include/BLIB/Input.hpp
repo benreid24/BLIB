1e3862de00cda4a6d89a53b83fd71b6f713f5ed7
Ben Reid <reidben24@gmail.com>
2022-11-04 00:44:10 -0400

    Fix build

--- include/BLIB/Input.hpp
+++ include/BLIB/Input.hpp
@@ -10,13 +10,11 @@
 #include <BLIB/Input/Actor.hpp>
 #include <BLIB/Input/Control.hpp>
 #include <BLIB/Input/DirectionalControl.hpp>
-#include <BLIB/Input/DispatchPolicy.hpp>
 #include <BLIB/Input/DispatchType.hpp>
 #include <BLIB/Input/InputSystem.hpp>
 #include <BLIB/Input/Joystick.hpp>
 #include <BLIB/Input/Listener.hpp>
 #include <BLIB/Input/MovementControl.hpp>
 #include <BLIB/Input/Trigger.hpp>
-#include <BLIB/Input/TriggerControl.hpp>
 
 #endif
