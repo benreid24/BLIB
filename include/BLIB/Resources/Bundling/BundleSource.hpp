920dd22fa19a75927e84e2091caaec66ee9a698c
Ben Reid <reidben24@gmail.com>
2022-11-28 23:56:58 -0500

    Start implementing resource bundler classes

---
+++ include/BLIB/Resources/Bundling/BundleSource.hpp
@@ -0,0 +1,36 @@
+#ifndef BLIB_RESOURCES_BUNDLING_BUNDLESOURCE_HPP
+#define BLIB_RESOURCES_BUNDLING_BUNDLESOURCE_HPP
+
+#include <string>
+
+namespace bl
+{
+namespace resource
+{
+namespace bundle
+{
+/**
+ * @brief Config struct defining a top level starting point for a bundle
+ *
+ * @ingroup Bundling
+ *
+ */
+struct BundleSource {
+    /// @brief The top level file or folder to start the bundle with
+    std::string sourcePath;
+
+    /// @brief Determines the behavior when sourcePath is a directory
+    enum Policy {
+        /// @brief All files are placed in the same bundle
+        BundleAllFiles,
+
+        /// @brief Each file gets its own bundle
+        CreateBundleForEachFile
+    } policy;
+};
+
+} // namespace bundle
+} // namespace resource
+} // namespace bl
+
+#endif
