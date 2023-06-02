41f313b70fd2022809af2fe7323e763adfe8c653
Ben Reid <reidben24@gmail.com>
2023-06-02 15:17:33 -0400

    Update GenericDrawableSystem for overlays

--- include/BLIB/Render/Systems/GenericDrawableSystem.hpp
+++ include/BLIB/Render/Systems/GenericDrawableSystem.hpp
@@ -9,6 +9,7 @@
 #include <BLIB/Render/Components/DrawableBase.hpp>
 #include <BLIB/Render/Components/SceneObjectRef.hpp>
 #include <BLIB/Render/Events/SceneDestroyed.hpp>
+#include <BLIB/Render/Overlays/Overlay.hpp>
 #include <BLIB/Render/Scenes/Scene.hpp>
 #include <BLIB/Render/Scenes/SceneObject.hpp>
 #include <BLIB/Render/Scenes/StagePipelines.hpp>
@@ -70,6 +71,32 @@ public:
                                        const scene::StagePipelines& pipelines);
 
     /**
+     * @brief Adds the component for the given entity to the overlay using the default pipelines
+     *
+     * @param entity The entity to add to the scene
+     * @param scene The scene to add the entity to
+     * @param descriptorUpdateFreq Expected update frequency of descriptors for this entity
+     * @param parent The entity to be the parent of the new overlay object
+     */
+    void addToOverlay(ecs::Entity, Overlay* overlay, UpdateSpeed descriptorUpdateFreq,
+                      ecs::Entity parent = ecs::InvalidEntity);
+
+    /**
+     * @brief Adds the component for the given entity to the overlay using custom pipelines for that
+     *        specific object
+     *
+     * @param entity The entity to add to the scene
+     * @param scene The scene to add the entity to
+     * @param descriptorUpdateFreq Expected update frequency of descriptors for this entity
+     * @param pipelines The pipelines to use for each scene stage when rendering this object
+     * @param parent The entity to be the parent of the new overlay object
+     */
+    void addToOverlayWithCustomPipelines(ecs::Entity, Overlay* overlay,
+                                         UpdateSpeed descriptorUpdateFreq,
+                                         const scene::StagePipelines& pipelines,
+                                         ecs::Entity parent = ecs::InvalidEntity);
+
+    /**
      * @brief Removes the entity from the scene that it is currently in
      *
      * @param entity The entity to remove from the scene
@@ -98,13 +125,15 @@ private:
         Scene* scene;
         UpdateSpeed updateFreq;
         scene::StagePipelines pipelines;
+        ecs::Entity parent;
 
         AddCommand(ecs::Entity ent, Scene* s, UpdateSpeed us,
-                   const scene::StagePipelines& pipelines)
+                   const scene::StagePipelines& pipelines, ecs::Entity parent = ecs::InvalidEntity)
         : entity(ent)
         , scene(s)
         , updateFreq(us)
-        , pipelines(pipelines) {}
+        , pipelines(pipelines)
+        , parent(parent) {}
     };
 
     std::mutex mutex;
@@ -155,6 +184,31 @@ void GenericDrawableSystem<T>::addToSceneWithCustomPipelines(
 }
 
 template<typename T>
+void GenericDrawableSystem<T>::addToOverlay(ecs::Entity ent, Overlay* scene,
+                                            UpdateSpeed descriptorUpdateFreq, ecs::Entity parent) {
+    addToOverlayWithCustomPipelines(ent, scene, descriptorUpdateFreq, stagePipelines, parent);
+}
+
+template<typename T>
+void GenericDrawableSystem<T>::addToOverlayWithCustomPipelines(
+    ecs::Entity entity, Overlay* scene, UpdateSpeed descriptorUpdateFreq,
+    const scene::StagePipelines& pipelines, ecs::Entity parent) {
+    std::unique_lock lock(mutex);
+    T* c = registry->getComponent<T>(entity);
+    if (!c) {
+#ifdef BLIB_DEBUG
+        BL_LOG_WARN << "Entity " << entity << " is missing component: " << typeid(T).name();
+#endif
+        return;
+    }
+    if (c->sceneRef.scene) {
+        if (c->sceneRef.scene == scene) { return; }
+        erased.emplace_back(c->sceneRef);
+    }
+    toAdd.emplace_back(entity, scene, descriptorUpdateFreq, pipelines, parent);
+}
+
+template<typename T>
 void GenericDrawableSystem<T>::doInit(engine::Engine&) {}
 
 template<typename T>
@@ -182,6 +236,8 @@ void GenericDrawableSystem<T>::init(engine::Engine& engine) {
 
 template<typename T>
 void GenericDrawableSystem<T>::update(std::mutex& frameMutex, float dt) {
+    std::unique_lock lock(mutex);
+
     if (!toAdd.empty() || !erased.empty()) {
         std::unique_lock lock(frameMutex);
 
@@ -198,7 +254,22 @@ void GenericDrawableSystem<T>::update(std::mutex& frameMutex, float dt) {
             }
             c->sceneRef.object = add.scene->createAndAddObject(
                 add.entity, c->drawParams, add.updateFreq, add.pipelines);
-            if (c->sceneRef.object) { c->sceneRef.scene = add.scene; }
+            if (c->sceneRef.object) {
+                c->sceneRef.scene = add.scene;
+                if (add.parent != ecs::InvalidEntity) {
+#ifdef BLIB_DEBUG
+                    Overlay* ov = dynamic_cast<Overlay*>(add.scene);
+                    if (!ov) {
+                        BL_LOG_ERROR << "Parent set for non-Overlay scene";
+                        continue;
+                    }
+#else
+                    Overlay* ov = static_cast<Overlay*>(add.scene);
+#endif
+
+                    ov->setParent(add.entity, add.parent);
+                }
+            }
             else {
                 BL_LOG_WARN << "Failed to add entity " << add.entity << " to scene " << add.scene;
             }
