cfcd5fa682e3b6ed10f5485e6b52a47c416eb8cd
Ben Reid <reidben24@gmail.com>
2023-10-08 01:55:09 -0400

    Implement shape outlines

--- src/Graphics/Shape2D.cpp
+++ src/Graphics/Shape2D.cpp
@@ -4,6 +4,15 @@ namespace bl
 {
 namespace gfx
 {
+namespace
+{
+glm::vec3 computeIntersect(const glm::vec3& p1, const glm::vec3& d1, const glm::vec3& p2,
+                           const glm::vec3& d2) {
+    const float b = (d1.x * (p2.y - p1.y) - d1.y * (p2.x - p1.x)) / (d1.y * d2.x - d1.x * d2.y);
+    return {p2.x + d2.x * b, p2.y + d2.y * b, 0.f};
+}
+} // namespace
+
 Shape2D::Shape2D()
 : Drawable()
 , OverlayScalable()
@@ -57,9 +66,8 @@ void Shape2D::update() {
     // determine required vertex and index counts
     const bool hasOutline           = outlineThickness != 0.f;
     const std::uint32_t pointCount  = getVertexCount();
-    const std::uint32_t vertexCount = pointCount * (hasOutline ? 2 : 1) + 1;
-    const std::uint32_t indexCount  = pointCount                          // center point
-                                     + pointCount * (hasOutline ? 5 : 3)  // shape points
+    const std::uint32_t vertexCount = pointCount * (hasOutline ? 3 : 1) + 1;
+    const std::uint32_t indexCount  = pointCount * (hasOutline ? 5 : 3)   // shape points
                                      + (hasOutline ? pointCount * 4 : 0); // outline points
     const std::uint32_t outlineStartIndex = pointCount + 1;
 
@@ -104,13 +112,60 @@ void Shape2D::update() {
 
     // populate indices for shape vertices
     for (unsigned int i = 1; i < outlineStartIndex; ++i) {
-        const unsigned int j = i * 3;
+        const unsigned int j = (i - 1) * 3;
         ib.indices()[j + 0]  = 0;
         ib.indices()[j + 1]  = i;
         ib.indices()[j + 2]  = (i == outlineStartIndex - 1) ? 1 : i + 1;
     }
 
-    // TODO - populate outline vertices
+    if (hasOutline) {
+        // populate outline vertices
+        const glm::vec3& centerPos = ib.vertices()[0].pos;
+        for (unsigned int i = 0; i < pointCount; ++i) {
+            auto& inner = ib.vertices()[i + pointCount + 1];
+            auto& outer = ib.vertices()[i + pointCount * 2 + 1];
+
+            inner.color = outlineColor;
+            outer.color = outlineColor;
+            inner.pos   = ib.vertices()[i + 1].pos; // same pos as shape corner
+
+            const auto computeExtrudedCenterPoint =
+                [this, &centerPos](const glm::vec3& cur, const glm::vec3& cmp) -> glm::vec3 {
+                const glm::vec3 center = (cur + cmp) / 2.f;
+                const glm::vec3 dir    = center - centerPos;
+                const float len        = glm::length(dir);
+                return centerPos + dir * ((len + outlineThickness) / len);
+            };
+
+            const glm::vec3& prevPoint = ib.vertices()[i == 0 ? pointCount : i].pos;
+            const glm::vec3& curPoint  = ib.vertices()[i + 1].pos;
+            const glm::vec3& nextPoint = ib.vertices()[i == pointCount - 1 ? 1 : i + 2].pos;
+            const glm::vec3 prevCenter = computeExtrudedCenterPoint(curPoint, prevPoint);
+            const glm::vec3 nextCenter = computeExtrudedCenterPoint(curPoint, nextPoint);
+            const glm::vec3 prevDir    = prevPoint - curPoint;
+            const glm::vec3 nextDir    = nextPoint - curPoint;
+            outer.pos                  = computeIntersect(prevCenter, prevDir, nextCenter, nextDir);
+        }
+
+        // populate outline indices
+        unsigned int ii = pointCount * 3;
+        for (unsigned int i = 0; i < pointCount; ++i) {
+            const unsigned int j  = i + pointCount + 1;
+            const unsigned int k  = i + pointCount * 2 + 1;
+            const unsigned int jn = (i == pointCount - 1) ? pointCount + 1 : j + 1;
+            const unsigned int kn = (i == pointCount - 1) ? pointCount * 2 + 1 : k + 1;
+
+            ib.indices()[ii + 0] = k;
+            ib.indices()[ii + 1] = j;
+            ib.indices()[ii + 2] = jn;
+
+            ib.indices()[ii + 3] = k;
+            ib.indices()[ii + 4] = jn;
+            ib.indices()[ii + 5] = kn;
+
+            ii += 6;
+        }
+    }
 
     component().commit();
 }
