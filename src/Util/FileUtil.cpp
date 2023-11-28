2e92ed65f0970f8cf37afc3be5812a467689ed7b
Ben Reid <reidben24@gmail.com>
2023-11-28 11:42:07 -0500

    Begin re-implementing FileUtl using std::filesystem

--- src/Util/FileUtil.cpp
+++ src/Util/FileUtil.cpp
@@ -5,6 +5,8 @@
 #include <BLIB/Util/FileUtil.hpp>
 
 #include <BLIB/Util/Random.hpp>
+#include <filesystem>
+
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
@@ -28,74 +30,47 @@ namespace bl
 {
 namespace util
 {
-namespace
-{
-void createDir(const std::string& path) {
-#ifdef BLIB_WINDOWS
-    _mkdir(path.c_str());
-#else
-    mkdir(path.c_str(), 0755);
-#endif
-}
-} // namespace
-
-bool FileUtil::exists(const std::string& file) {
-    struct stat info;
-    if (stat(file.c_str(), &info) != 0) return false;
-    return info.st_mode & S_IFREG;
-}
+bool FileUtil::exists(const std::string& file) { return std::filesystem::is_regular_file(file); }
 
 bool FileUtil::directoryExists(const std::string& file) {
-    struct stat info;
-    if (stat(file.c_str(), &info) != 0) return false;
-    return info.st_mode & S_IFDIR;
+    return std::filesystem::is_directory(file);
 }
 
 bool FileUtil::isBigEndian() {
     union {
-        uint32_t i;
+        std::uint32_t i;
         char c[4];
     } endTest = {0x01020304};
     return endTest.c[0] == 1;
 }
 
 std::string FileUtil::getExtension(const std::string& file) {
-    std::size_t i = file.find_last_of(".");
-    if (i != std::string::npos) return file.substr(i + 1);
-    i = file.find_last_of("/\\");
-    if (i != std::string::npos) return file.substr(i + 1);
-    return file;
+    std::string ext = std::filesystem::path(file).extension();
+    if (ext.size() >= 1 && ext[0] == '.') { ext.erase(0, 1); }
+    return ext;
 }
 
 std::string FileUtil::getBaseName(const std::string& file) {
-    std::string base = getFilename(file);
-    std::size_t i    = base.find_last_of(".");
-    if (i != std::string::npos) base.erase(i);
-    return base;
+    return std::filesystem::path(file).stem();
 }
 
 std::string FileUtil::getFilename(const std::string& file) {
-    const std::size_t i = file.find_last_of("/\\");
-    if (i != std::string::npos) return file.substr(i + 1);
-    return file;
+    return std::filesystem::path(file).filename();
 }
 
 std::string FileUtil::getPath(const std::string& file) {
-    const std::size_t i = file.find_last_of("/\\");
-    return file.substr(0, (i != std::string::npos) ? (i + 1) : (i));
+    return std::filesystem::path(file).parent_path();
 }
 
 std::string FileUtil::joinPath(const std::string& l, const std::string& r) {
     if (l.empty()) return r;
     if (r.empty()) return l;
 
-    std::size_t ls = std::string::npos;
-    if (l[l.size() - 1] == '/' || l[l.size() - 1] == '\\') ls = l.size() - 1;
-
-    std::size_t rs = 0;
-    if (r[0] == '/' || r[0] == '\\') rs = 1;
-
-    return l.substr(0, ls) + "/" + r.substr(rs);
+    const bool rabs = r[0] == '/' || r[0] == '\\';
+    const std::string_view rv(rabs ? r.c_str() + 1 : r.c_str(), rabs ? r.size() - 1 : r.size());
+    auto result = std::filesystem::path(l);
+    result /= std::filesystem::path(rv);
+    return result;
 }
 
 bool FileUtil::startsWithPath(const std::string& file, const std::string& path) {
@@ -119,54 +94,37 @@ std::string FileUtil::genTempName(const std::string& path, const std::string& ex
 
 void FileUtil::copyFile(const std::string& src, const std::string& dest) {
     if (src == dest) return;
-
-    std::ifstream source(src.c_str(), std::ios::binary);
-    std::ofstream dst(dest.c_str(), std::ios::binary);
-
-    std::istreambuf_iterator<char> begin_source(source);
-    std::istreambuf_iterator<char> end_source;
-    std::ostreambuf_iterator<char> begin_dest(dst);
-    std::copy(begin_source, end_source, begin_dest);
+    std::filesystem::copy_file(src, dest);
 }
 
 bool FileUtil::createDirectory(const std::string& path) {
-    if (directoryExists(path)) return true;
-
-    std::string cd;
-    cd.reserve(path.size());
-    for (unsigned int i = 0; i < path.size(); ++i) {
-        if (path[i] == '/' || path[i] == '\\') { createDir(cd); }
-        cd.push_back(path[i]);
-    }
-    createDir(cd);
-    return directoryExists(cd);
+    return std::filesystem::create_directories(path);
 }
 
 std::vector<std::string> FileUtil::listDirectory(const std::string& path, const std::string& ext,
                                                  bool recursive) {
     if (path.empty()) return {};
 
-    struct dirent* cfile;
     std::vector<std::string> list;
-    std::string folder = path;
-    if (folder[folder.size() - 1] != '/' && folder[folder.size() - 1] != '\\')
-        folder.push_back('/');
 
-    DIR* cd = opendir(folder.c_str());
-    if (cd != nullptr) {
-        while ((cfile = readdir(cd))) {
-            const std::string file = cfile->d_name;
-            const std::string full = joinPath(folder, file);
-            if (!directoryExists(full)) {
-                if (ext.empty() || getExtension(file) == ext) list.push_back(full);
-            }
-            else if (recursive) {
-                if (file != "." && file != "..") {
-                    const std::vector<std::string> files = listDirectory(full, ext, true);
-                    list.insert(list.end(), files.begin(), files.end());
-                }
-            }
+    const auto append = [&list, &ext, &path](const std::filesystem::path& cpath) {
+        bool add = true;
+        if (!ext.empty()) {
+            const auto e = cpath.extension();
+            const std::string_view es(e.empty() ? e.c_str() : e.c_str() + 1,
+                                      e.empty() ? 0 : std::distance(e.begin(), e.end()) - 1);
+            add = ext == es;
         }
+        if (add) { list.emplace_back(std::filesystem::relative(cpath, path)); }
+    };
+
+    if (recursive) {
+        for (const auto& cpath : std::filesystem::recursive_directory_iterator(path)) {
+            append(cpath);
+        }
+    }
+    else {
+        for (const auto& cpath : std::filesystem::directory_iterator(path)) { append(cpath); }
     }
 
     return list;
