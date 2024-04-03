6df596f68924f8f747171b5b2647791c235115ca
Ben Reid <reidben24@gmail.com>
2024-04-03 18:09:15 -0400

    Implement DescriptorFactory for particles

---
+++ examples/Particles/DescriptorFactory.hpp
@@ -0,0 +1,34 @@
+#ifndef DESCRIPTORFACTORY_HPP
+#define DESCRIPTORFACTORY_HPP
+
+#include "DescriptorSet.hpp"
+#include <BLIB/Render/Descriptors/DescriptorSetFactory.hpp>
+
+class DescriptorFactory : public bl::rc::ds::DescriptorSetFactory {
+public:
+    virtual ~DescriptorFactory() = default;
+
+private:
+    bl::engine::Engine* engine;
+
+    virtual void init(bl::engine::Engine& e, bl::rc::Renderer& renderer) override {
+        engine = &e;
+
+        bl::rc::vk::DescriptorPool::SetBindingInfo bindings;
+        bindings.bindingCount = 1;
+
+        bindings.bindings[0].binding         = 0;
+        bindings.bindings[0].descriptorCount = 1;
+        bindings.bindings[0].descriptorType  = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+        bindings.bindings[0].stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+
+        descriptorSetLayout = renderer.vulkanState().descriptorPool.createLayout(bindings);
+    }
+
+    virtual std::unique_ptr<bl::rc::ds::DescriptorSetInstance> createDescriptorSet()
+        const override {
+        return std::make_unique<DescriptorSet>(*engine, descriptorSetLayout);
+    }
+};
+
+#endif
--- examples/Particles/main.cpp
+++ examples/Particles/main.cpp
@@ -5,6 +5,15 @@
 #include "Particle.hpp"
 #include "Renderer.hpp"
 
+/**
+ * TODO
+ *   - Make pipeline + shaders
+ *   - ECS drawable component (templated? may not need to be)
+ *   - Build main()
+ *   - Add particles. Start static to test rendering then use particle system to make interesting
+ *   - Template DescriptorSet and move into library
+ */
+
 int main() {
     // TODO
     return 0;
