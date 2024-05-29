d593d7af86335405553d34464dcd3ae18fe05f03
Ben Reid <reidben24@gmail.com>
2024-05-29 18:39:47 -0400

    Add option to fix macos build

--- cmake/configure_blib_target.cmake
+++ cmake/configure_blib_target.cmake
@@ -74,6 +74,7 @@ function(configure_blib_target target_name)
             target_compile_options(${target_name} PUBLIC
                 -Wno-nullability-extension
                 -Wno-nullability-completeness
+                -Wno-error=unused-command-line-argument
             )
         endif()
     endif()
